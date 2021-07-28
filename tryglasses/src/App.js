2fa41c3428439f45256418e44205f64641666cf5
VanHien1234 <ghienduahau@gmail.com>
2021-07-28 23:33:23 +0700

    tryglass

---
+++ tryglasses/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
+import DanhSachKinh from './DanhSachKinh/DanhSachKinh';
+
+function App() {
+  return (
+    <div className="App">
+      <DanhSachKinh/>
+    </div>
+  );
+}
+
+export default App;
